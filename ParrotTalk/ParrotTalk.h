de4151df30cd5076b3c3d17614ef20c015393c2e
Jupiter Jones <jupiter.jones@mail.com>
2017-11-09 11:44:06 +1100

    Add XCode project for ParrotTalk.framework

---
+++ ParrotTalk/ParrotTalk.h
@@ -0,0 +1,19 @@
+//
+//  ParrotTalk.h
+//  ParrotTalk
+//
+//  Created by Jupiter on 9/11/17.
+//  Copyright © 2017 MyNation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParrotTalk.
+FOUNDATION_EXPORT double ParrotTalkVersionNumber;
+
+//! Project version string for ParrotTalk.
+FOUNDATION_EXPORT const unsigned char ParrotTalkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParrotTalk/PublicHeader.h>
+
+
